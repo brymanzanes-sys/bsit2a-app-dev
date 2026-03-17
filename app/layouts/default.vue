929b9f3eb6ecb29ba8ac6220496a6579502d39ad
brymanzanes-sys <brymanzanes@gmail.com>
2026-03-17 10:29:50 +0800

    Added my project

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -21,7 +21,9 @@
         <v-list density="compact" nav>
           <v-list-item prepend-icon="mdi-home-city" title="Home" value="home"></v-list-item>
           <v-list-item prepend-icon="mdi-account" title="My Account" value="account"></v-list-item>
-          <v-list-item prepend-icon="mdi-account-group-outline" title="Users" value="users"></v-list-item>
+          <v-list-item prepend-icon="mdi-account-group-outline" title="Remedial" value="remedial"
+          to="/remedial"
+          ></v-list-item>
         </v-list>
       </v-navigation-drawer>
 
